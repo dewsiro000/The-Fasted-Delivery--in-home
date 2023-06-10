69d972cc3cc1abe95a6c3ff60200dc3cf2fc769e
dewsiro000 <norawit1267@gmail.com>
2023-06-10 20:17:32 +0700

    user newproduct

--- frontend/src/page/Newproduct.js
+++ frontend/src/page/Newproduct.js
@@ -1,23 +1,37 @@
 import React from 'react'
+import { BsCloudUpload } from 'react-icons/bs'
 
 const Newproduct = () => {
     return (
-        <div className='m-auto w-full max-w-md p-4'>
-            <div>
-                <form>
-                    <lable htmlFor='name'>Name</lable>
-                    <input type={"text"} name="name" />
+        <div className='p-4'>
+            <form className='m-auto w-full max-w-md shadow flex flex-col p-3 bg-white'>
+                <lable htmlFor='name'>Name</lable>
+                <input type={"text"} name="name" className='bg-slate-200 p-1 my-1' />
 
-                    <select>
-                        <option>Fruits</option>
-                        <option>Vegetable</option>
-                        <option>Icream</option>
-                        <option>Dosa</option>
-                        <option>Piza</option>
-                    </select>
-                </form>
-            </div>
+                <label htmlFor='category'>Category</label>
+                <select className='bg-slate-200 p-1 my-1' id='category'>
+                    <option>Fruits</option>
+                    <option>Vegetable</option>
+                    <option>Icream</option>
+                    <option>Dosa</option>
+                    <option>Piza</option>
+                </select>
+
+                <lable htmlFor='image'>Image</lable>
+                <div id='image' className='h-40 w-full bg-slate-200 rounded flex items-center justify-center'>
+                    <span className='text-5xl'><BsCloudUpload /></span>
+                </div>
+
+                <label htmlFor='price' className='my-1'>Price</label>
+                <input type={"text"} className='bg-slate-200 p-1 my-1' />
+
+                <label htmlFor='description'>Description</label>
+                <textarea rows={2} className='bg-slate-200 p-1 my-1 resize-none'></textarea>
+
+                <button className='bg-red-500 hover:bg-red-600 text-white text-lg my-2 font-medium drop-shadow '>Save</button>
+            </form>
         </div>
+
     )
 }
 
