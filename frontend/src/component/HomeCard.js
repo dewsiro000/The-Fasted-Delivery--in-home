ca81267e30f7043776c86532b0d0181f74020d81
dewsiro000 <norawit1267@gmail.com>
2023-06-24 00:18:49 +0700

    image loading

--- frontend/src/component/HomeCard.js
+++ frontend/src/component/HomeCard.js
@@ -4,17 +4,30 @@ const HomeCard = ({ name, image, category, price, loading }) => {
     return (
         <div className='bg-white shadow-md p-2 rounded min-w-[150px]'>
             {
-                name && <>
-                    <div className='w-40 h-40'>
-                        <img src={image} className='h-full w-full' />
+                name ? (
+                    <>
+                        <div className='w-40 h-40'>
+                            <img src={image} className='h-full w-full' />
+                        </div>
+                        <h3 className='font-semibold text-slate-600 text-center capitalize text-lg'>
+                            {name}
+                        </h3>
+                        <p className='text-center text-slate-500 font-medium'>
+                            {category}
+                        </p>
+                        <p className='text-center font-bold'>
+                            <span className='text-red-500'>₹</span>
+                            <span>{price}</span>
+                        </p>
+                    </>
+                ) : (
+                    <div className="flex justify-center items-center h-full">
+                        <p>{loading}</p>
                     </div>
-                    <h3 className='font-semibold text-slate-600 text-center capitalize text-lg'>{name}</h3>
-                    <p className='text-center text-slate-500 font-medium'>{category}</p>
-                    <p className='text-center font-bold'><span className='text-red-500'>₹</span><span>{price}</span></p>
-                </>
-            }
+                )}
         </div>
     )
+
 }
 
 export default HomeCard
